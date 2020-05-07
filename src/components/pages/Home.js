59cea55415bc2c07467817a50269aaa42c161f83
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-05-06 21:52:04 -0700

    home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,191 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Button, Card } from "react-bootstrap";
 import logo from "./image/logo.PNG";
 import "../../index.css";
 import { Input } from "reactstrap";
-import "./Donate.css";
-function Home() {
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import './style.css';
+import './Boxes.css';
+
+
+
+const Home = () => {
+
+  useEffect(() => {
+    Aos.init({ duration: 2000 });
+  }, []);
+
   return (
     <div>
-      <h2>TEST MAIN PAGE</h2>
-      <p>
-        What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing
-        and typesetting industry. Lorem Ipsum has been the industry's standard
-        dummy text ever since the 1500s, when an unknown printer took a galley
-        of type and scrambled it to make a type specimen book. It has survived
-        not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with
-        the release of Letraset sheets containing Lorem Ipsum passages, and more
-        recently with desktop publishing software like Aldus PageMaker including
-        versions of Lorem Ipsum
-      </p>
-      <Button variant="primary">Primary</Button>{" "}
-      <Button variant="secondary">Secondary</Button>{" "}
-      <Button variant="success">Success</Button>{" "}
-      <Button variant="warning">Warning</Button>{" "}
-      <Button variant="danger">Danger</Button>{" "}
-      <Button variant="info">Info</Button>{" "}
-      <Button variant="light">Light</Button>{" "}
-      <Button variant="dark">Dark</Button> <Button variant="link">Link</Button>
-      <p>
-        Where does it come from? Contrary to popular belief, Lorem Ipsum is not
-        simply random text. It has roots in a piece of classical Latin
-        literature from 45 BC, making it over 2000 years old. Richard
-        McClintock, a Latin professor at Hampden-Sydney College in Virginia,
-        looked up one of the more obscure Latin words, consectetur, from a Lorem
-        Ipsum passage, and going through the cites of the word in classical
-        literature, discovered the undoubtable source. Lorem Ipsum comes from
-        sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The
-        Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a
-        treatise on the theory of ethics, very popular during the Renaissance.
-        The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes
-        from a line in section 1.10.32. The standard chunk of Lorem Ipsum used
-        since the 1500s is reproduced below for those interested. Sections
-        1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are
-        also reproduced in their exact original form, accompanied by English
-        versions from the 1914 translation by H. Rackham.
-      </p>
-      <Card style={{ width: "rem" }}>
-        <Card.Img variant="top" src={logo} />
-        <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-          <Button variant="primary">Go somewhere</Button>
-        </Card.Body>
-      </Card>
-      <br></br>
-      <br></br>
-      <Button variant="outline-primary">Primary</Button>{" "}
-      <Button variant="outline-secondary">Secondary</Button>{" "}
-      <Button variant="outline-success">Success</Button>{" "}
-      <Button variant="outline-warning">Warning</Button>{" "}
-      <Button variant="outline-danger">Danger</Button>{" "}
-      <Button variant="outline-info">Info</Button>{" "}
-      <Button variant="outline-light">Light</Button>{" "}
-      <Button variant="outline-dark">Dark</Button>
-      <Card style={{ width: "25rem" }}>
-        <Card.Img variant="top" src={logo} />
-        <a>
-          <div className="service-head1 text-center">
-            <h2>
-              {" "}
-              How to Donate
-              <div className="comment1"> </div>
-            </h2>
-            <h3>
-              {" "}
-              <span className="line">
-                {" "}
-                <div className="border1"></div>
-              </span>
-            </h3>
-          </div>
-        </a>
-        <Card.Body>
-          <Card.Text>
-            <div className="flexbox">
-              <div className="search">
-                <h1>Search for a charity of your choice:</h1>
-                {/* <h3>Click on search icon, then type your keyword.</h3> */}
-                <div>
-                  <input
-                    type="text"
-                    placeholder="       Search . . ."
-                    required
-                  />
-                </div>
-              </div>
-            </div>
-          </Card.Text>
-          <div className="donation-buttons">
-            <Button
-              style={{
-                marginLeft: "20px",
-                backgroundColor: "green",
-                border: "white",
-                color: "white",
-              }}
-            >
-              $10
-            </Button>
-            <Button
-              style={{
-                marginLeft: "20px",
-                backgroundColor: "green",
-                border: "white",
-                color: "white",
-              }}
-            >
-              $25
-            </Button>
-            <Button
-              style={{
-                marginLeft: "20px",
-                backgroundColor: "green",
-                border: "white",
-                color: "white",
-              }}
-            >
-              $50
-            </Button>
-            <Button
-              style={{
-                marginLeft: "20px",
-                backgroundColor: "green",
-                border: "white",
-                color: "white",
-              }}
-            >
-              $100
-            </Button>
-          </div>
-          <Button
-            style={{
-              marginTop: "15px",
-              marginLeft: "20px",
-              backgroundColor: "green",
-              border: "white",
-              color: "white",
-            }}
-          >
-            $
-          </Button>
-          <Input
-            variant="primary"
-            style={{
-              width: "225px",
-              marginLeft: "60px",
+      <div className="home-news" style={{ backgroundColor: "#B09A70" }}>
+        <p>
+          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like). It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </p>
+      </div>
+      <div className="home-div">
+        <h1>OUR MISSION:</h1>
+        <div data-aos="fade-up" className="boxes">
+          <p>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </p>
+        </div>
 
-              height: "35px",
-              borderRadius: "30px",
-              border: "3px solid black",
-              padding: "15px",
-            }}
-          ></Input>
-          <Button
-            style={{
-              marginTop: "15px",
-              marginLeft: "20px",
-              backgroundColor: "green",
-              border: "white",
-              color: "white",
-            }}
-          >
-            Give Now
-          </Button>
-          <Button>
-            <img
-              className="d-block w-50"
-              src={require("../images/paypal.png")}
-              alt="Paypal"
-            />
-          </Button>
-        </Card.Body>
-      </Card>
-    </div>
+        <div data-aos="fade-up" className="boxes">
+          <h1 style={{ fontStyle: "italic", color: "green" }}>COMPASSION</h1>
+          <h4>We believe that all perons deserve to be treated with respect, compassion and dignity.</h4>
+        </div>
+
+        <div data-aos="fade-up" className="boxes">
+          <h1>OPTIMISM</h1>
+          <h4>We believe the ability of people to strive for better lives, and we help provide them wth the means to achieve their dreams.</h4>
+        </div>
+
+        <div data-aos="fade-left" className="boxes">
+          <h1>INTEGRITY</h1>
+          <h4>We will ensure honesty and fairness in all our actions. We will always do the right thing.</h4>
+        </div>
+
+        <div data-aos="fade-right" className="boxes">
+          <h2>RESPONSIBILITY</h2>
+          <h4>We will hold ourselves to a high standard of accountability and honor every donation commitment. We will never take any donation for personal means.</h4>
+        </div>
+
+
+        <div data-aos="fade-up" className="boxes">
+
+          <h2>COMMUNITY</h2>
+          <h4>We are building a community and network to empower others and give back to those in need.</h4>
+
+        </div>
+
+      </div>
+    </div >
   );
 }
 
