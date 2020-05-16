ad3a4273d9c583698f26724cbafa4b4f166f21f8
Lloyd Marcelino <lloydmarcelino@gmail.com>
2020-05-16 16:59:38 -0700

    responsive

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,8 +4,14 @@ import Aos from 'aos';
 import 'aos/dist/aos.css';
 import './style.css';
 import './Boxes.css';
-import { Image, Col, Row, Container, InputGroup, FormControl, Button } from 'react-bootstrap'
-import Kids from '../images/charity.png';
+import { Col, Row, Container, InputGroup, FormControl, Button } from 'react-bootstrap'
+import Kids from '../images/kids.png';
+import Boy from '../images/boy.png';
+import Girl from '../images/girl.png';
+import RedHand from '../images/redhand.png';
+import HeartHand from '../images/hearthand.png';
+import Kids2 from '../images/container.png';
+
 import { UserAddOutlined, DollarCircleOutlined, BarChartOutlined } from '@ant-design/icons'
 import { Link } from "react-router-dom";
 
@@ -21,71 +27,105 @@ const Home = (props) => {
   return (
     <div>
 
-      <div className="normal" data-aos="zoom-in" data-aos-delay="100" style={{ backgroundColor: "#FFFFFF", paddingTop: "100px", paddingBottom: "100px" }}>
+      <div className="normal" data-aos="fade-down" data-aos-delay="100" style={{ backgroundColor: "#FFFFFF", paddingTop: "100px", paddingBottom: "100px" }}>
         <div className="page_inner_div">
-          <p style={{ fontSize: "60px", fontWeight: "300", color: "#66615b", marginTop: "30px" }}>Donationally</p>
-          <p style={{ fontSize: "20px", color: "#66615b", paddingLeft: "70px", paddingRight: "70px" }}>
+          <h1 style={{ fontWeight: "900", color: "#263238", marginTop: "30px" }} id="responsive_h1" data-aos="zoom-in" data-aos-delay="100">Donationally</h1>
+          <h7 style={{ color: "#66615b" }} id="responsive_h7" data-aos="fade-right" data-aos-delay="100">
             is a non-profit charity search and donation
             application, helping users in multiple ways to stay informed about
-            world events and opportunities to give back to those in need. </p>
+            world events and opportunities to give back to those in need. </h7>
+          <div className="row" style={{ paddingTop: "70px" }}>
+            <div className="col-md-6" data-aos="fade-right" data-aos-delay="100">
+              <img src={HeartHand} style={{ border: "10px solid white" }} fluid className="only" />
+            </div>
+            <div className="col-md-6" data-aos="fade-left" data-aos-delay="100">
+              <img src={RedHand} style={{ border: "10px solid white" }} fluid className="only" />
+            </div>
+          </div>
         </div>
 
       </div>
-      <div className="normal" data-aos="fade-up" style={{ backgroundColor: "#D8C1AB", paddingLeft: "300px", paddingRight: "300px", paddingTop: "100px", paddingBottom: "100px" }}>
-        <div className="row">
+      <div className="normal" data-aos="fade-up" data-aos-delay="100" style={{ backgroundColor: "#D8C1AB", paddingTop: "100px", paddingBottom: "100px" }}>
+        <div className="page_innver_div">
 
-          <div class="col-lg-6 pt-3 pt-lg-0 content" data-aos="fade-right" data-aos-delay="100" >
-            <Image src={Kids} fluid style={{ border: "10px solid white" }} />
-          </div>
-          <div class="col-lg-6 pt-3 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
-            <h1 style={{ fontWeight: "900", color: "#607d8b", marginTop: "30px" }}>Our Mission</h1>
-            <p style={{ fontSize: "20px", color: "white", paddingBottom: "20px" }}>
-              is to create lasting change one donation at a time.</p>
-            <p style={{ fontSize: "20px", color: "white", paddingBottom: "20px" }}>We <span style={{ fontSize: "50px", color: "#607d8b" }}>envision</span> a world everyone has the opportunity to live a better life.</p>
-          </div>
+
+
+          <h1 style={{ fontWeight: "900", color: "#607d8b", marginTop: "30px" }} id="responsive_h1">Our Mission</h1>
+          <h7 id="responsive_h7" style={{ color: "white" }}>
+            is to create lasting change one donation at a time.</h7>
+          <br></br>
+          <h7 id="responsive_h7" style={{ color: "white" }}> We <span id="responsive_h1" style={{ color: "#607d8b" }}>envision</span> a world everyone has the opportunity to live a better life.</h7>
 
         </div>
+
       </div>
 
 
-      <div className="normal" data-aos="fade-up" style={{ backgroundColor: "#7A9E9F", paddingLeft: "300px", paddingRight: "300px", paddingTop: "100px", paddingBottom: "100px" }}>
+      <div className="normal" data-aos="fade-up" style={{ backgroundColor: "#7A9E9F", paddingTop: "100px", paddingBottom: "100px" }}>
 
         <div className="page_inner_div">
-          <h1 style={{ fontWeight: "900", color: "white", marginTop: "30px" }}>Our Core Values</h1>
-          <Container fluid>
-            <Row style={{ paddingTop: "30px" }}>
-              <Col data-aos="zoom-in" data-aos-delay="100" style={{ padding: "0 20px 0 20px" }}>
-                <h5 style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat" }}>COMPASSION</h5>
-                <p style={{ color: "white", fontSize: "18px" }}>We believe that all perons deserve to be treated with respect, compassion and dignity.</p>
-              </Col>
-              <Col data-aos="zoom-in" data-aos-delay="100" style={{ padding: "0 20px 0 20px" }}>
-                <h5 style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat" }}>OPTIMISM</h5>
-                <p style={{ color: "white", fontSize: "18px" }}>We believe the ability of people to strive for better lives, and we help provide them wth the means to achieve their dreams.</p>
-              </Col>
-              <Col data-aos="zoom-in" data-aos-delay="100" style={{ padding: "0 20px 0 20px" }}>
-                <h5 style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat" }}>INTEGRITY</h5>
-                <p style={{ color: "white", fontSize: "18px" }}>We will ensure honesty and fairness in all our actions. We will always do the right thing.</p>
-              </Col>
-            </Row>
-            <Row style={{ padding: "50px" }}>
-              <Col data-aos="zoom-in" data-aos-delay="100" style={{ padding: "0 35px 0 35px" }}>
-                <h5 style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat" }}>RESPONSIBILITY</h5>
-                <p style={{ color: "white", fontSize: "18px" }}>We will hold ourselves to a high standard of accountability and honor every donation commitment. We will never take any donation for personal means.</p>
-              </Col>
-              <Col data-aos="zoom-in" data-aos-delay="100" style={{ padding: "0 35px 0 35px" }}>
-                <h5 style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat" }}>COMMUNITY</h5>
-                <p style={{ color: "white", fontSize: "18px" }}>We are building a community and network to empower others and give back to those in need.</p>
-              </Col>
-            </Row>
-          </Container>
+          <h1 style={{ fontWeight: "900", color: "white", marginTop: "30px" }} id="responsive_h1">Our Core Values</h1>
+
+
+          <div className="row" style={{ paddingTop: "30px" }}>
+
+
+
+            <div className="col-md-4" style={{ padding: "0 70px 0 70px" }}>
+              <h7 id="responsive_h7" style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat", paddingTop: "20px" }}>COMPASSION</h7>
+              <p id="responsive_h7" style={{ color: "white", paddingBottom: "20px" }}>We believe that all perons deserve to be treated with respect, compassion and dignity.</p>
+            </div>
+            <div className="col-md-4" style={{ padding: "0 70px 0 70px" }}>
+              <h7 id="responsive_h7" style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat", paddingTop: "10px" }}>INTEGRITY</h7>
+              <p id="responsive_h7" style={{ color: "white" }}>We will ensure honesty and fairness in all our actions. We will always do the right thing.</p>
+            </div>
+            <div className="col-md-4" style={{ padding: "0 70px 0 70px" }}>
+              <h7 id="responsive_h7" style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat" }}>COMMUNITY</h7>
+              <p id="responsive_h7" style={{ color: "white" }}>We are building a community and network to empower others and give back to those in need.</p>
+            </div>
+
+
+
+            <div className="row">
+              <div className="col-md-6" style={{ padding: "20px 0 20px 0" }} >
+                <img src={Boy} fluid style={{ border: "10px solid white", width: "75%", height: "auto" }} className="only" />
+              </div>
+              <div className="col-md-6" style={{ paddingBottom: "20px", paddingTop: "20px" }}>
+                <section>
+                  <img src={Girl} fluid style={{ border: "10px solid white", width: "65%", height: "auto" }} className="only" />
+                  <br></br>
+                </section>
+                <section style={{ paddingTop: "30px" }}>
+                  <img src={Kids2} fluid style={{ border: "10px solid white", width: "65%", height: "auto" }} className="only" />
+                </section>
+              </div>
+            </div>
+
+
+          </div>
+
+
+          <div className="row" style={{ paddingTop: "50px" }}>
+
+            <div className="col-md-6" data-aos="zoom-in" data-aos-delay="100" style={{ padding: "0 90px 0 90px" }}>
+              <h7 id="responsive_h7" style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat" }}>OPTIMISM</h7>
+              <p id="responsive_h7" style={{ color: "white" }} id="responsive_h7">We believe the ability of people to strive for better lives, and we help provide them wth the means to achieve their dreams.</p>
+            </div>
+            <div className="col-md-6" data-aos="zoom-in" data-aos-delay="100" style={{ padding: "0 90px 0 90px" }}>
+              <h7 id="responsive_h7" style={{ fontStyle: "italic", color: "white", fontWeight: "700", fontFamily: "Montserrat" }}>RESPONSIBILITY</h7>
+              <p style={{ color: "white" }} id="responsive_h7">We will hold ourselves to a high standard of accountability and honor every donation commitment. We will never take any donation for personal means.</p>
+            </div>
+
+          </div>
+
 
         </div>
 
       </div>
-      <div className="normal" style={{ backgroundColor: "#FFFFFF", paddingLeft: "300px", paddingRight: "300px", paddingTop: "100px", paddingBottom: "100px" }}>
+      <div className="normal" style={{ backgroundColor: "#FFFFFF", paddingTop: "100px", paddingBottom: "100px" }}>
 
-        <h1 style={{ fontWeight: "900", color: "#66615b", marginTop: "30px" }} data-aos="left" data-aos-delay="100" >Join Us!</h1>
-        <p style={{ fontSize: "20px", color: "#66615b", padding: "10px 0 30px 0" }} data-aos="right" data-aos-delay="100">
+        <h1 id="responsive_h1" style={{ fontWeight: "900", color: "#66615b", marginTop: "30px" }} data-aos="left" data-aos-delay="100" >Join Us!</h1>
+        <p id="responsive_h7" style={{ color: "#66615b", padding: "10px 90px 30px 90px" }} data-aos="right" data-aos-delay="100">
           Members of Donationally will receive numerous perks on their
           philanthropist journey. Benefits include: information and updates on
           "Favorite" charities, personal donation history, and so much more.
@@ -113,11 +153,11 @@ const Home = (props) => {
         </div>
 
       </div>
-      <div className="normal" data-aos="fade-up" style={{ backgroundColor: "#81d4fa", paddingLeft: "300px", paddingRight: "300px", paddingTop: "100px", paddingBottom: "100px" }}>
+      <div className="normal" data-aos="fade-up" style={{ backgroundColor: "#81d4fa", paddingTop: "100px", paddingBottom: "100px" }}>
 
         <div className="page_inner_div">
-          <h1 style={{ fontWeight: "900", color: "#263238" }}>Donate now!</h1>
-          <p style={{ fontSize: "20px", color: "white", marginBottom: "0px" }}>
+          <h1 id="responsive_h1" style={{ fontWeight: "900", color: "#263238" }}>Donate now!</h1>
+          <p id="responsive_h7" style={{ fontSize: "20px", color: "white", marginBottom: "0px" }}>
             Discover a variety of worldwide charities using the Charity Map.
             This process will geolocate by user to encourage local donation
             opportunities. You can also type in a custom area around the globe
